95831c1ee3d1be627e2ef0e140a5d0ee8ceed8f7
Bruno Duarte <frank.bruno555@gmail.com>
2015-09-25 20:40:42 -0300

    Startando versionamento

---
+++ modulos/01-intoducao/Introducao.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        if (phpversion() >= 5.4):
+            echo phpversion() . " Olá mundo, prodemos programar";
+        else:
+            echo phpversion() . " Olá mundo, preciso atualizar o PHP";
+        endif;
+        ?>
+    </body>
+</html>
