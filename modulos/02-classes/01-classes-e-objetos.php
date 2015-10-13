121d11fe08bcac5b7e394d623d40d9f92f5977c0
Bruno Duarte <frank.bruno555@gmail.com>
2015-10-13 18:17:31 -0300

    inicializacao do respositorio

---
+++ modulos/02-classes/01-classes-e-objetos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        require './class/ClassesObjetos.class.php';
+        
+        $teste = new ClassesObjetos();
+        $teste->getClass('de introducao', 'de mostrar uma classe');
+        ?>
+    </body>
+</html>
---
+++ modulos/02-classes/class/ClassesObjetos.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class ClassesObjetos {
+    
+    var $Classe; 
+    var $Funcao;
+    
+    function getClass($Classe, $Funcao){
+        echo "<p> A Classe {$Classe} serve para {$Funcao}";
+    }
+    function verClass(){
+        echo '<pre>';
+        print_r($this);
+        echo '</pre>';
+    }
+    
+}
