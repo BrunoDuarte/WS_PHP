52bffa18d73890a90f6085d98fa007c2cc5fe5dd
Bruno Duarte <frank.bruno555@gmail.com>
2015-11-10 20:10:54 -0300

    aula de stored procedures

--- modulos/07-php-data-object/03-stered-procedures.php
+++ modulos/07-php-data-object/03-stered-procedures.php
@@ -7,12 +7,38 @@
     <body>
         <?php
         require './_app/Config.inc.php';
-
+        
+        $Conn = new Conn();
+        
         try {
+            
+            $Query = "Select * from ws_siteviews_agent where agent_name = :name";
+            $Exec = $Conn->getConn()->prepare($Query);
+            
+            $Exec->bindValue(":name", 'Chrome');
+            $Exec->execute();
+            
+            $Chrome = $Exec->fetchAll(PDO::FETCH_ASSOC); // ou apenas fetch se retornar apenas um resultado
+            
+            $Exec->bindValue(":name", 'Safari');
+            $Exec->execute();
+            
+            $Safari = $Exec->fetch(PDO::FETCH_ASSOC);
             
         } catch (PDOException $e) {
             PHPErro($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
         }
+        
+        if($Chrome):
+//            var_dump($Chrome);
+            echo "{$Chrome[0]['agent_name']} tem {$Chrome[0]['agent_views']} visita(s)<hr>";
+        endif;
+        
+        if($Safari):
+//            var_dump($Safari);
+            echo "{$Safari['agent_name']} tem {$Safari['agent_views']} visita(s)<hr>";
+        endif;
+        
         ?>
     </body>
 </html>
