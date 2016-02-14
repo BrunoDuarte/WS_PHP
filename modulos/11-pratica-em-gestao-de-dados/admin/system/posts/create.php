f84593912409cdecd8fcf044961f9bde7eb13f8f
Bruno Duarte <frank.bruno555@gmail.com>
2016-02-14 12:11:26 -0300

    estruturando post controller

--- modulos/11-pratica-em-gestao-de-dados/admin/system/posts/create.php
+++ modulos/11-pratica-em-gestao-de-dados/admin/system/posts/create.php
@@ -7,9 +7,11 @@
         </header>
 
         <?php
-        EchoMsg("Erro ao cadastrar:", "Existem campos ogrigatórios sem preencher.", ALERT);
-        EchoMsg("Erro ao cadastrar:", "A capa deve ser JPG, PNG ou GIF com até 2MB!.", ALERT);
-        EchoMsg("Sucesso:", "Seu post foi cadastrado com sucesso. <a target=\"_blank\" href=\"../artigo/titulo\">Ver Post</a>", ACCEPT);
+        $post = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        if (isset($post) && $post['SendPostForm']):
+
+            var_dump($post);
+        endif;
         ?>
 
 
@@ -17,37 +19,78 @@
 
             <label class="label">
                 <span class="field">Enviar Capa:</span>
-                <input type="file" name="cover" />
+                <input type="file" name="post_cover" />
             </label>
 
             <label class="label">
                 <span class="field">Titulo:</span>
-                <input type="text" name="post_title" />
+                <input type="text" name="post_title" value="<?php if (isset($post['post_title'])) echo $post['post_title']; ?>" />
             </label>
 
             <label class="label">
                 <span class="field">Conteúdo:</span>
-                <textarea class="js_editor" name="post_content" rows="10"></textarea>
+                <textarea class="js_editor" name="post_content" rows="10"><?php if (isset($post['post_content'])) echo htmlspecialchars($post['post_content']); ?></textarea>
             </label>
 
             <div class="label_line">
 
                 <label class="label_small">
                     <span class="field">Data:</span>
-                    <input type="text" class="formDate center" name="post_title" value="<?= date('d/m/Y H:i:s'); ?>" />
+                    <input type="text" class="formDate center" name="post_date" value="<?php
+                    if (isset($post['post_date'])): echo $post['post_date'];
+                    else: echo date('d/m/Y H:i:s');
+                    endif;
+                    ?>" />
                 </label>
 
                 <label class="label_small">
                     <span class="field">Categoria:</span>
                     <select name="post_category">
                         <option value=""> Selecione a categoria: </option>
+                        <?php
+                        $readSes = new Read();
+                        $readSes->ExeRead("ws_categories", "where category_parent is null order by category_title asc");
+                        if ($readSes->getRowCount() >= 1):
+                            foreach ($readSes->getResult() as $ses):
+                                echo "<option disabled=\"disable\" value=\"\"> {$ses['category_title']} </option>";
+                                $readCat = new Read();
+                                $readCat->ExeRead("ws_categories", "where category_parent = :parent order by category_title asc", "parent={$ses['category_id']}");
+                                if ($readCat->getRowCount() >= 1):
+                                    foreach ($readCat->getResult() as $cat):
+                                        echo "<option ";
+
+                                        if ($post['post_category'] == $cat['category_id']):
+                                            echo "selected=\"selected\" ";
+                                        endif;
+
+                                        echo "value=\"{$cat['category_id']}\"> &raquo;&raquo; {$cat['category_title']} </option>";
+                                    endforeach;
+                                endif;
+                            endforeach;
+                        endif;
+                        ?>
                     </select>
                 </label>
 
                 <label class="label_small">
                     <span class="field">Author:</span>
-                    <select name="post_category">
-                        <option value=""> <?= "{$_SESSION['userlogin']['user_name']} {$_SESSION['userlogin']['user_lastname']}"; ?> </option>
+                    <select name="post_author">
+                        <option value="<?= $_SESSION['userlogin']['user_id']; ?>"> <?= "{$_SESSION['userlogin']['user_name']} {$_SESSION['userlogin']['user_lastname']}"; ?> </option>
+                        <?php
+                        $readAut = new Read();
+                        $readAut->ExeRead("ws_users", "where user_id != :id and user_level >= :level order by user_name asc", "id={$_SESSION['userlogin']['user_id']}&level=2");
+                        if ($readAut->getRowCount() >= 1):
+                            foreach ($readAut->getResult() as $aut):
+                                echo "<option ";
+
+                                if ($post['post_author'] == $aut['user_id']):
+                                    echo "selected=\"selected\"";
+                                endif;
+
+                                echo "value=\"{$aut['user_id']}\"> {$aut['user_name']} {$aut['user_lastname']} </option>";
+                            endforeach;
+                        endif;
+                        ?>
                     </select>
                 </label>
 
@@ -60,19 +103,19 @@
                     <input type="file" multiple name="gallery_covers" />
                 </label>
 
-                <ul class="gallery">
-                    <?php for ($i = 1; $i <= 10; $i++): ?>
+                <ul class="gallery" style="display: none">
+<?php for ($i = 1; $i <= 10; $i++): ?>
                         <li<?php if ($i % 5 == 0) echo ' class="right"'; ?>>
                             <div class="img thumb_small"></div>
                             <a class="del" href="#delete">Deletar</a>                    
                         </li>
-                    <?php endfor; ?>
+<?php endfor; ?>
                 </ul>                
             </div>
 
 
             <input type="submit" class="btn blue" value="Cadastrar" name="SendPostForm" />
-            <input type="submit" class="btn green" value="Cadastrar & Publicar" name="SendPostFormPublish" />
+            <input type="submit" class="btn green" value="Cadastrar & Publicar" name="SendPostForm" />
 
         </form>
 
